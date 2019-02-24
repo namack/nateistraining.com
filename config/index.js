5f779789dba2f01e0bb2be0c27bd7b883e5dd12f
Nate Amack <nate@amack.org>
2019-02-24 08:26:34 -0700

    some initial customization

--- config/index.js
+++ config/index.js
@@ -1,12 +1,12 @@
 module.exports = {
   pathPrefix: '/', // Prefix for all links. If you deploy your site to example.com/portfolio your pathPrefix should be "portfolio"
 
-  siteTitle: 'Minimal Blog', // Navigation and Site Title
-  siteTitleAlt: 'Minimal Blog - Gatsby Starter', // Alternative Site title for SEO
-  siteTitleManifest: 'MinimalBlog',
-  siteUrl: 'https://minimal-blog.lekoarts.de', // Domain of your site. No trailing slash!
+  siteTitle: 'Nate is Training', // Navigation and Site Title
+  siteTitleAlt: 'Nate is Training: What is Nate doing? He is training.', // Alternative Site title for SEO
+  siteTitleManifest: 'NateIsTraining',
+  siteUrl: 'https://nateistraining.com', // Domain of your site. No trailing slash!
   siteLanguage: 'en', // Language Tag on <html> element
-  siteHeadline: 'Writing and publishing content', // Headline for schema.org JSONLD
+  siteHeadline: 'What is Nate doing? He is training', // Headline for schema.org JSONLD
   siteBanner: '/social/banner.jpg', // Your image for og:image tag. You can find it in the /static folder
   favicon: 'src/favicon.png', // Your image for favicons. You can find it in the /src folder
   siteDescription: 'Minimal Blog with a focus on typography. Gatsby Starter powered by MDX.', // Your site description
@@ -14,10 +14,10 @@ module.exports = {
   siteLogo: '/social/logo.png', // Image for schemaORGJSONLD
 
   // siteFBAppID: '123456789', // Facebook App ID - Optional
-  userTwitter: '@minimal', // Twitter Username - Optional
+  userTwitter: '@nate_amack', // Twitter Username - Optional
   ogSiteName: 'minimal', // Facebook Site Name - Optional
   ogLanguage: 'en_US', // Facebook Language
-  googleAnalyticsID: 'UA-47519312-6',
+  googleAnalyticsID: 'UA-50498805-5',
 
   // Manifest and Progress color
   // See: https://developers.google.com/web/fundamentals/web-app-manifest/
